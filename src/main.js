a4f195c24a99f78e8c845ea582fb939c66402aab
Felipe Caputo <felipecaputo90@gmail.com>
2018-07-27 09:35:45 -0300

    use formate, export change

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 const winston = require('winston');
 const Sentry = require('raven');
 
+const logFormat = winston.format.printf(info => {
+  return `${info.timestamp} [${info.level}]: ${info.message}`;
+});
+
 function getLogger() {
   const transports = [
     new winston.transports.Console({
-      level: 'silly',
-      format: winston.format.timestamp(),
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        logFormat,
+      ),
       stderrLevels: ['error'],
       consoleWarnLevels: ['warn']
     }),
@@ -25,4 +33,4 @@ function getLogger() {
   return winston.createLogger({ transports });
 }
 
-module.exports = getLogger;
+module.exports = getLogger();
