3aee79eb2ab0edf2b870122f89ef5f6a0109ebcb
= <leandrotk100@gmail.com>
2021-02-15 12:49:22 -0300

    Fix sentry node type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,5 @@
 declare module quintoandar_logger {
-
-    type Sentry = import('@sentry/node').Sentry
+    type Sentry = typeof import('@sentry/node')
     type SentryParams = import('@sentry/node').NodeOptions
 
     type LoggerMethod = import('winston').LeveledLogMethod
@@ -12,15 +11,16 @@ declare module quintoandar_logger {
         readonly active: boolean;
     }
 
-
     function getLogger(mod: NodeModule): Logger
     function setTracer(newTracer: TracerBase): QuintoLogger
     function startSentry(newSentryParams: SentryParams): QuintoLogger
     function startSentry(newSentryParams: SentryParams, sentryFunc: SentryFunc): QuintoLogger
+    
     interface QuintoLogger {
         getLogger: typeof getLogger
         setTracer: typeof setTracer
         startSentry: typeof startSentry
     }
 }
+
 export = quintoandar_logger
